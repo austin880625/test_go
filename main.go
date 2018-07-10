e2d745958de31b64c1a1dfb98dbdf4a1ba042253
Austin Chang <austin880625@gmail.com>
2018-07-10 23:39:18 +0800

    added a message when server starts added deployment configuration in template

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    fmt.Println("The server is ready.")
     http.HandleFunc("/", handler)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
